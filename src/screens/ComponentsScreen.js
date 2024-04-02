67c7fd31f81d564d72eebfb8e856081ad86eb52f
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-04-03 05:54:35 +0600

    Using View and js variable to show in ui

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
     // return <Text style={styles.textStyle}>This is the components screen</Text>;
-    return <Text style={{ fontSize: 20 }}>This is the components screen</Text>;
+    const greeting = <Text>Hello to you!</Text>;
+    const name = 'Sajeet';
+    return (
+        <View>
+            <Text style={{ fontSize: 20 }}>This is the components screen</Text>
+            {greeting}
+            <Text style={styles.textStyle}>My name is {name}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
