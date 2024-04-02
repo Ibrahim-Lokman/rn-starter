6fca200ad3f4fb4510898a8d341bc618bf38606f
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-04-03 05:28:16 +0600

    Creates custom components and use StyleSheet.

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ComponentsScreen = () => {
+    return <Text style={styles.textStyle}>This is the components screen</Text>;
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 25
+    }
+});
+
+export default ComponentsScreen;
